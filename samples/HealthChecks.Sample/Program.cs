2f9b4485489e8e155a9385ec54b851cac475113d
cleverman48 <first1124.dev.master@gmail.com>
2023-12-15 23:10:03 +0900

    checked start

--- samples/HealthChecks.Sample/Program.cs
+++ samples/HealthChecks.Sample/Program.cs
@@ -7,6 +7,7 @@ public class Program
     public static void Main(string[] args)
     {
         CreateWebHostBuilder(args).Build().Run();
+        
     }
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
